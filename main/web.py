ed3177b98b6dbbb2bfdc28a031e6440030dd7522
yangzteL <965579738@qq.com>
2018-04-28 17:54:50 +0800

    web

---
+++ main/web.py
@@ -0,0 +1,18 @@
+
+
+def application(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/html')])
+    body = '<h1>Hello, %s!</h1>' % (environ['PATH_INFO'][1:] or 'web')
+    return [body.encode('utf-8')]
+
+
+
+# 从wsgiref模块导入:wsgiref只考虑能运行，不考虑效率
+from wsgiref.simple_server import make_server
+
+
+# 创建一个服务器，IP地址为空，端口是8000，处理函数是application:
+httpd = make_server('', 8000, application)
+print('Serving HTTP on port 8000...')
+# 开始监听HTTP请求:
+httpd.serve_forever()
